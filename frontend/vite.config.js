fcffacdd2e5512a8e6018e00150a2bd270128fe3
shlok <majmundarshlok06@gmail.com>
2025-08-10 11:19:44 +0530

    clerk added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'process.env': process.env
+  }
+});
